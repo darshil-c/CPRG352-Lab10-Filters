f96372a4886a43e38496b22ba2a36146ffad4b64
Chaudhari <Chaudhari@DESKTOP-M3C7DC6>
2021-07-14 15:00:41 -0600

    Added AdminFilter Functionality Added AdminFilter functionality, still need to fix admin redirect.

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -5,6 +5,7 @@
  */
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -15,6 +16,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Role;
+import models.User;
 
 /**
  *
@@ -29,6 +35,31 @@ public class AdminFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        HttpSession session = httpRequest.getSession();
+        String email = (String) session.getAttribute("email");
+        
+        UserDB udb = new UserDB();
+        User user = udb.get(email);
+        
+        Role role = user.getRole();
+        int roleID = role.getRoleId();
+        
+        System.out.print(roleID);
+        
+        if (roleID == 1) {
+            httpResponse.sendRedirect("admin");
+            return;
+        } else if (roleID == 2) {
+            httpResponse.sendRedirect("notes");
+            return;
+        } else if (roleID != 1 || roleID != 2) {
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        
         chain.doFilter(request, response);
     }
 
